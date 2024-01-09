f04920cac0641c8771dd270a8cc64973bcb74a82
Gaurav kumar singh <137489749+gauravsinghBCA@users.noreply.github.com>
2024-01-09 20:13:26 +0530

    Add files via upload

---
+++ KeyCodeProject/jscript.js
@@ -0,0 +1,27 @@
+const container = document.getElementById("keycontainer");
+// container.innerHTML = generateHTML("-","-","-");
+window.addEventListener("keydown", (e) =>{
+container.innerHTML = generateHTML(e.key,e.code,e.key.charCodeAt(0)) 
+});
+
+function generateHTML(key,code,keyCode){
+    return  `
+    <div class="key-container">
+      <h4>key</h4>
+        <div class="key-content"> ${key === " " ? "space" : key}
+        </div>
+    </div>
+
+    <div class="key-container">
+      <h4>code</h4>
+        <div class="key-content"> ${code}
+        </div>
+    </div>
+
+    <div class="key-container">
+      <h4>key code</h4>
+        <div class="key-content"> ${keyCode}
+        </div>
+    </div>
+    `;
+}
